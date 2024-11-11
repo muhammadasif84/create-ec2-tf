2e9e2ad29a6ed74358018a7adac7177d487b673f
muhammadasif84 <mohammedasif80902@gmail.com>
2024-11-12 04:16:39 +0500

    created security-group

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # create-ec2-tf
 
-Creating full features AWS EC2 instance using Terraform
+Creating full features AWS EC2 instance using terraform
 <br/>
 Lab-1   => creating basic-ec2
 <br/>
-Creating basic AWS ec2-instance using terraform.
+Creating basic AWS ec2-instance using terraform
 <br/>
 lab-2   => creating ssh-key
 <br/>
 Creating ssh-key pairs using terraform
+<br/>
+Lab-3   => creating security-group
+<br/>
+Creating aws security-group using terraform
 
--- instance.tf
+++ instance.tf
@@ -10,6 +10,32 @@ output "key-name" {
   value = aws_key_pair.key-pair-ec2.key_name
 }
 
+#creating security-group for an instance
+resource "aws_security_group" "security-group-ec2" {
+  name        = "security-group-ec2"
+  description = "allowed ports"
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 3306, 2701]
+    iterator = port
+    content {
+
+
+      description = "Creating security group"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+#security-group id output
+output "sg-id" {
+  value = aws_security_group.security-group-ec2.id # "sg-047493b8078ad5696"
+
+}
+
 #creating basic ec2-instance
 resource "aws_instance" "aws-ec2" {
 
